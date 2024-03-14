09b664a0a7d3279b576b817f4b6f3846d75b1541
Elanis <elanis@hotmail.com>
2024-03-14 09:47:51 +0100

    Refacto

---
+++ src/business/monTraitementPourBody.js
@@ -0,0 +1,5 @@
+export default function monTraitementPourBody(foo) {
+	let bar = foo + 1;
+
+	return bar; 
+}
--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+import monTraitementPourBody from '../../business/monTraitementPourBody.js';
+
 import './index.css';
 
 export default function Body() {
@@ -13,19 +15,13 @@ export default function Body() {
 	const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 	const elements = text.split(' ');
 
-	function monTraitement(foo) {
-		let bar = foo + 1;
-
-		return bar; 
-	}
-
 	return (
 		<div className="body">
 			<p>Compteur: {count}</p>
 			<p>Compteurs: {count + count2}</p>
 
 			<input type="button" onClick={() => setCount((currCount) => currCount + 1)} value="Plus!" />
-			<input type="button" onClick={() => setCount(monTraitement)} value="Plus (l'autre)!" />
+			<input type="button" onClick={() => setCount(monTraitementPourBody)} value="Plus (l'autre)!" />
 			<input type="button" onClick={() => setCount2((currCount) => currCount + 1)} value="Plus (count2)!" />
 
 			<ul>
